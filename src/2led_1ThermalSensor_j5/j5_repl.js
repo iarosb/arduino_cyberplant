412660c4e5d27dca4d56d5d1000ef9510d72466a
Iaroslav Blinkov <here2contactme@gmail.com>
2021-12-01 17:09:57 +0200

    package.json added; repl changed

--- src/2led_1ThermalSensor_j5/j5_repl.js
+++ src/2led_1ThermalSensor_j5/j5_repl.js
@@ -1,7 +1,6 @@
 // arduino UNO
 // two leds (red, green)
 // firmata standard uploaded to the board
-
 const five = require("johnny-five");
 const board = new five.Board({
   repl:true
@@ -10,13 +9,8 @@ const board = new five.Board({
 const danger_PIN = 6;
 const normal_PIN = 4;
 
-board.on("connect", ()=> {
-  console.log("Connected");
-});
-
 board.on("ready", () => {
 
-    console.log('Ready');
   const thermometer = new five.Thermometer({
     controller: "LM35",
     pin: "A1",
@@ -43,36 +37,36 @@ board.on("ready", () => {
 
 // TODO: refactor util functions
 // separate files(modules) where j5 is used only here to handle hardware
-  function signalOnTemp(c) {
-    if (isTempSafe(c))
-    {
-      normal_LED.on();
-      board.wait(9000, () => {
-      normal_LED.off();
-     })
-    }
-    else
-    {
-      danger_LED.on();
-      board.wait(9000, () => {
-      danger_LED.off();
-     })
-    }
-   };
-   
-   function isTempSafe(temp) {
-     if (temp < 13.0 || temp > 25.0) {
-        return false;
-     } else {
-        return true;
-     }
-   };
-   
-   function logTemperature(c, f) {
-     console.log("Celsius: ", c);
-     console.log("Farhenheit: ", f);
-     console.log("----------------------------");
-   };
+function signalOnTemp(c) {
+  if (isTempSafe(c))
+  {
+    normal_LED.on();
+    board.wait(9000, () => {
+    normal_LED.off();
+   })
+  }
+  else
+  {
+    danger_LED.on();
+    board.wait(9000, () => {
+    danger_LED.off();
+   })
+  }
+ };
+
+ function isTempSafe(temp) {
+   if (temp < 13.0 || temp > 25.0) {
+      return false;
+   } else {
+      return true;
+   }
+ };
+
+ function logTemperature(c, f) {
+   console.log("Celsius: ", c);
+   console.log("Farhenheit: ", f);
+   console.log("----------------------------");
+ };
 
    board.on("exit", ()=> {
     normal_LED.off();
